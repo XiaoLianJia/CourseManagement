9855cd86b0e480b33afa1f753a4e1df7e87710f8
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-10-18 14:02:55 +0800

    +课时信息控制层接口框架

---
+++ server/src/main/java/com/course/management/dto/LessonDto.java
@@ -0,0 +1,16 @@
+package com.course.management.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *     课时信息数据传输类
+ * </p>
+ * @author zhangbin
+ * @date 2019-10-18
+ */
+@Data
+public class LessonDto implements Serializable {
+}
