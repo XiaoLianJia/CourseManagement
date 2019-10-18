8ff5ac8566eedf85e2dd358ddfdf74c70665bf24
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-10-18 14:06:26 +0800

    +授课人员信息控制层接口框架

---
+++ server/src/main/java/com/course/management/dto/LecturerDto.java
@@ -0,0 +1,16 @@
+package com.course.management.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *     授课人员信息数据传输类
+ * </p>
+ * @author zhangbin
+ * @date 2019-10-18
+ */
+@Data
+public class LecturerDto implements Serializable {
+}
