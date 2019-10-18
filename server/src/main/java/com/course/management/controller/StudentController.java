83146efb839bb7f2edcfb0d66ddfc767d8c24bcb
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-10-18 14:11:20 +0800

    +学生信息控制层接口框架

--- server/src/main/java/com/course/management/controller/StudentController.java
+++ server/src/main/java/com/course/management/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.course.management.controller;
 
-import com.course.management.dto.LecturerDto;
+import com.course.management.dto.StudentDto;
 import com.course.management.http.PageRequest;
 import com.course.management.http.Response;
 import com.github.pagehelper.PageInfo;
@@ -13,36 +13,36 @@ import java.util.List;
 
 /**
  * <p>
- *     授课人员信息控制层
+ *     学生信息控制层
  * </p>
  * @author zhangbin
  * @date 2019-10-18
  */
 @Slf4j
 @RestController
-@RequestMapping("lecturer")
-public class LecturerController {
+@RequestMapping("student")
+public class StudentController {
 
 	@RequestMapping("add")
-	public Response add(@RequestBody LecturerDto lecturerDto) {
+	public Response add(@RequestBody StudentDto studentDto) {
 
 		return Response.success();
 	}
 
 	@RequestMapping("remove")
-	public Response remove(@RequestBody LecturerDto lecturerDto) {
+	public Response remove(@RequestBody StudentDto studentDto) {
 
 		return Response.success();
 	}
 
 	@RequestMapping("update")
-	public Response update(@RequestBody LecturerDto lecturerDto) {
+	public Response update(@RequestBody StudentDto studentDto) {
 
 		return Response.success();
 	}
 
 	@RequestMapping("page")
-	public Response<List<LecturerDto>> page(@RequestBody PageRequest<LecturerDto> pageRequest) {
+	public Response<List<StudentDto>> page(@RequestBody PageRequest<StudentDto> pageRequest) {
 
 		PageInfo pageInfo = new PageInfo();
 		return Response.pageSuccess(pageInfo);
