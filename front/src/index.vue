d102c41eafe349f4de2454b30ee252b710be0355
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-10-19 17:48:26 +0800

    +前端工程

---
+++ front/src/index.vue
@@ -0,0 +1,17 @@
+<style>
+    /* h2 {
+        color: red;
+    } */
+</style>
+
+<template>
+    <h2>Hello, World!</h2>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {};
+    }
+}
+</script>
