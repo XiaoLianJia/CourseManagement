ee1356e899531e8a427a25e4a03aa5e80fa7632e
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-10-27 20:01:45 +0800

    *部分修改

--- server/src/main/java/com/course/management/exception/ExceptionAdvice.java
+++ server/src/main/java/com/course/management/exception/ExceptionAdvice.java
@@ -1,6 +1,5 @@
 package com.course.management.exception;
 
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
