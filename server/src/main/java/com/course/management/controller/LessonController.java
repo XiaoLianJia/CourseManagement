e86d1a298464eb95cd77b69991b4e31d56906cab
m_zhangbin@foxmail.com <m_zhangbin@foxmailc.om>
2019-11-03 10:48:26 +0800

    *分页查询

--- server/src/main/java/com/course/management/controller/LessonController.java
+++ server/src/main/java/com/course/management/controller/LessonController.java
@@ -1,13 +1,18 @@
 package com.course.management.controller;
 
 import com.course.management.model.Lesson;
+import com.course.management.repository.LessonRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 /**
@@ -21,6 +26,9 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("lesson")
 public class LessonController {
+
+    @Autowired
+    private LessonRepository lessonRepository;
 
     @RequestMapping("add")
 	public String add(@Valid @ModelAttribute Lesson lesson,
@@ -37,8 +45,23 @@ public class LessonController {
     }
 
     @RequestMapping("lessonInfo")
-    public ModelAndView lessonInfo() {
+    public ModelAndView lessonInfo(HttpServletRequest request) {
+        int page = 0;
+        if (null != request.getParameter("page")
+                && ! request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        int size = 8;
+        if (null != request.getParameter("size")
+                && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+        Page<Lesson> lessons = lessonRepository.findAll(PageRequest.of(page, size));
+
         ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("lessons", lessons);
         modelAndView.setViewName("lesson/lessonInfo");
         return modelAndView;
     }
